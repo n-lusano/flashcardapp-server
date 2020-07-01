1b7151d2ca3fd722d13824fb4e1d362ad44a33db
N-LUSANO <n.lusano@gmail.com>
2020-07-01 20:36:34 +0200

    Added new POST endpoints for sessions and collections

--- routers/collections.js
+++ routers/collections.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const Collection = require("../models").collection;
 const Card = require("../models").card;
+const authMiddleware = require("../auth/middleware");
 
 const router = new Router();
 
@@ -27,6 +28,18 @@ router.get("/:id", async (req, res, next) => {
       return res.status(404).send();
     }
     res.status(200).send(collection);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", authMiddleware, async (req, res, next) => {
+  try {
+    const userId = req.user.dataValues["id"];
+    const { name } = req.body;
+
+    const collection = await Collection.create({ userId, name });
+    res.send(collection);
   } catch (error) {
     next(error);
   }
